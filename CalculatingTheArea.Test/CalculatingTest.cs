6bf4b89698844be8ac68affd216bbb1cc7550a5d
carefreeeee <104973824+carefreeeee@users.noreply.github.com>
2022-05-18 10:51:12 +0300

    Add files via upload

---
+++ CalculatingTheArea.Test/CalculatingTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using Microsoft.Analytics.UnitTest;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+
+namespace CalculatingTheArea.Test
+{
+
+    [TestFixture]
+    public class CalculatingTest
+    {
+        [Test]
+        public void Circle()
+        {
+            Assert.AreEqual(3.14, Calculate.CalculateAreaCircle(1));
+            Assert.AreEqual(12.57, Calculate.CalculateAreaCircle(2));
+            Assert.AreEqual(0, Calculate.CalculateAreaCircle(0));
+            Assert.Throws<ArgumentException>(() => Calculate.CalculateAreaCircle(-1));
+            Assert.Throws<ArgumentException>(() => Calculate.CalculateAreaCircle(-2));
+        }
+
+        [Test]
+        public void Triangle()
+        {
+            Assert.IsTrue(Calculate.CheckingRightTriangle(3, 4, 5));
+            Assert.IsTrue(Calculate.CheckingRightTriangle(6, 8, 10));
+            Assert.IsFalse(Calculate.CheckingRightTriangle(2, 2, 3));
+
+            Assert.Throws<ArgumentException>(() => Calculate.CheckingRightTriangle(3, 4, -5));
+            Assert.Throws<ArgumentException>(() => Calculate.CheckingRightTriangle(-3, 4, 5));
+            Assert.Throws<ArgumentException>(() => Calculate.CheckingRightTriangle(3, -4, 5));
+
+
+            Assert.AreEqual(1.98, Calculate.CalculateAreaTriangle(2, 2, 3));
+            Assert.AreEqual(6, Calculate.CalculateAreaTriangle(3, 4, 5));
+
+            Assert.Throws<ArgumentException>(() => Calculate.CalculateAreaTriangle(1, 2, 3));
+            Assert.Throws<ArgumentException>(() => Calculate.CalculateAreaTriangle(2, -2, 3));
+        }
+    }
+
+
+}
---
+++ CalculatingTheArea/Calculate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingTheArea
+{
+    /// <summary>
+    /// Класс для рассчета площадей фигур
+    /// </summary>
+    public class Calculate
+    {
+        /// <summary>
+        /// Вычисление площади круга по заданному радиусу
+        /// </summary>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static double CalculateAreaCircle(double radius)
+        {
+            return radius >= 0 ? Math.Round((Math.PI * radius * radius),2) : throw new ArgumentException("Недопустимое значение");
+        }
+
+        /// <summary>
+        /// Вычисление площади треугольника по трем сторонам
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static double CalculateAreaTriangle(double a, double b, double c)
+        {
+            if (VerificationTriangle(a, b, c))
+            {
+                var p = (a + b + c) / 2;
+                return Math.Round(Math.Sqrt(p * (p - a) * (p - b) * (p - c)),2);
+            }
+            else throw new ArgumentException("Недопустимое значение"); 
+        }
+
+        /// <summary>
+        /// Проверка треугольника на прямоугольность по трем сторонам
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static bool CheckingRightTriangle(double a, double b, double c)
+        {
+            if (VerificationTriangle(a, b, c))
+            {
+                if (a * a ==  b * b + c * c || b * b == a * a + c * c || c * c == b * b + a * a)
+                    return true;
+                return false;
+            }
+            else throw new ArgumentException("Недопустимое значение");
+        }
+        
+        /// <summary>
+        /// Проверка на существование треугольника
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        static bool VerificationTriangle(double a, double b, double c)
+        {
+            return (a > 0 && b > 0 && c > 0 && a < b + c && b < a + c && c < b + a) ? true : false;
+        }
+
+    }
+}
